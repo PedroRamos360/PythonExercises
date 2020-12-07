71cd503f37f3eed24a08a6f5ab73767b5ee9d6aa
Pedro Ramos <53490820+PedroRamos360@users.noreply.github.com>
2020-12-07 18:38:23 -0300

    finalização do problema stay on a chess board

--- 03_DailyInterview_30112020.py
+++ 03_DailyInterview_30112020.py
@@ -1,5 +1,7 @@
 # Staying on a chess board
 
+import random
+
 def is_move_possible(move):
     if move[0] > 0 and move[1] > 0:
         return True
@@ -8,25 +10,37 @@ def is_move_possible(move):
 
 
 def is_knight_on_board(x, y, k, cache={}):
-    all_moves = []
-    possible_moves = []
+    out_of_board_probability = 1
+    moves = [(x, y)]
+    for i in range(k):
+        for move in moves:
+            all_moves = []
+            possible_moves = []
+            move_x, move_y = move
+            all_moves.append((move_x - 1, move_y - 2))
+            all_moves.append((move_x - 2, move_y - 1))
+            all_moves.append((move_x + 2, move_y - 1))
+            all_moves.append((move_x - 2, move_y + 1))
+            all_moves.append((move_x + 1, move_y - 2))
+            all_moves.append((move_x - 1, move_y + 2))
+            all_moves.append((move_x + 2, move_y + 1))
+            all_moves.append((move_x + 1, move_y + 2))
 
-    all_moves.append((x - 1, y - 2))
-    all_moves.append((x - 2, y - 1))
-    all_moves.append((x + 2, y - 1))
-    all_moves.append((x - 2, y + 1))
-    all_moves.append((x + 1, y - 2))
-    all_moves.append((x - 1, y + 2))
-    all_moves.append((x + 2, y + 1))
-    all_moves.append((x + 1, y + 2))
+            filter_object = filter(is_move_possible, all_moves)
+            for possible_move in filter_object:
+                possible_moves.append(possible_move)
+            moves = []
+            for possible_move in possible_moves:
+                moves.append(possible_move)
+            
+            if possible_move != []:
+                out_of_board_probability *= len(possible_moves)/len(all_moves)   
+            else:
+                out_of_board_probability *= 1/len(all_moves)
+        
+    return out_of_board_probability
 
-    moves = filter(is_move_possible, all_moves)
-    for move in moves:
-        possible_moves.append(move)
-    
-    return len(possible_moves)/len(all_moves)
-
-print(is_knight_on_board(0, 0, 1))
+print(is_knight_on_board(0, 0, 8))
 # 0.25
 
 # OBS: Só funciona para k=1
